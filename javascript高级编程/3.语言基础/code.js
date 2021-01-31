b1d1815da851a5911fc96aac8c59e41969df4223
Heron <heronwang@qq.com>
2021-01-31 21:25:21 +0800

    add:第三章内容

---
+++ javascript高级编程/3.语言基础/code.js
@@ -0,0 +1,18 @@
+
+let a;
+console.log(typeof  a);
+
+a=undefined;
+console.log(typeof  a);
+
+a=null;
+console.log(typeof(a));
+
+a = true;
+console.log(typeof  a);
+
+a=12;
+console.log(typeof  a);
+
+a={}
+console.log(typeof  a);
